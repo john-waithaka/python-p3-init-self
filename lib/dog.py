0e3d945a4cbec95fa5d1aaede6a84ca298cc0591
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-07-15 12:42:08 +0300

    success on init

--- lib/dog.py
+++ lib/dog.py
@@ -7,3 +7,10 @@ class Dog:
     def __init__(self, name, breed="Mutt"):
         self.name = name
         self.breed = breed
+
+nara = Dog("Nara" , "German Shepherd")
+print(f"{nara.name} is a {nara.breed}.")        
+
+
+# fido = Dog("Fido", "Golden Retriever")
+# print(f"{fido.name} is a {fido.breed}.")
